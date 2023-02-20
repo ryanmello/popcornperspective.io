b5fb45d4038d63102135bca2a1e7537c7612f408
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-20 08:20:25 -0800

    student form

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,6 +3,12 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 export default function Student() {
+
+  const[name, setName] = React.useState('')
+  const[address, setAddress] = React.useState('')
+
+
+
   return (
     <Box
       component="form"
@@ -12,9 +18,14 @@ export default function Student() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="standard-basic" label="Student Name" variant="standard" />
-      <TextField id="standard-basic" label="Student Address" variant="standard" />
-
+      <TextField id="standard-basic" label="Student Name" variant="standard" 
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      />
+      <TextField id="standard-basic" label="Student Address" variant="standard" 
+      value={address}
+      onChange={(e) => setAddress(e.target.value)}
+      />
     </Box>
   );
 }
