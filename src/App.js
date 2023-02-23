8c0cce1a93bd4a6af9382eac4f2d14dcba412cd1
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-23 12:08:32 -0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './layout/Navbar';
+
+
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
