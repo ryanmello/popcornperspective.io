617bb7402472fc2e73cc09bda956121a1ff04151
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-23 11:45:52 -0800

    application clean

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
-import AppBar from './components/Appbar';
-import Student from './components/Student';
-
 function App() {
   return (
     <div className="App">
-      <AppBar/>
-      <Student/>
+      
     </div>
   );
 }
--- src/components/Appbar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function AppBar() {
-  
-  return (
-    <h1>Spring Boot React Application</h1>
-  );
-}
