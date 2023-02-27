6a89bcacf8982b0b6676d40344a4f03af1d81d02
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-26 17:16:54 -0800

    add user to database from frontend add user form

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,29 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function AddUser() {
+
+  let navigate = useNavigate()
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { name, username, email } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8084/users", user)
+    navigate("/");
+  };
+
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-6 offset-md-3 border rounded mt-2 shadow'>
-          <h2 className='text-center m-4'>Register User</h2>
-          <div className='mb-3'>
-            <label htmlFor='Name' className='form-label'>Name</label>
-            <input type={"text"} className="form-control" name='name'/>
-          </div>
-          <div className='mb-3'>
-            <label htmlFor='Name' className='form-label'>Username</label>
-            <input type={"text"} className="form-control" name='name'/>
-          </div>
-          <div className='mb-3'>
-            <label htmlFor='Name' className='form-label'>Email</label>
-            <input type={"text"} className="form-control" name='name'/>
-          </div>
-          <div className='pb-3 text-center'>
-            <button type='submit' className='btn btn-outline-primary m-2'>Submit</button>
-            <button type='submit' className='btn btn-outline-danger'>Cancel</button>
-          </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+          <form onSubmit={(e)=>onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Username
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Email
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className="pb-3 text-center">
+              <button type="submit" className="btn btn-outline-primary m-2">
+                Submit
+              </button>
+              <button type="submit" className="btn btn-outline-danger">
+                Cancel
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
