cae95f663a570e480b38e01b8bfd048f51c46070
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-27 08:34:30 -0800

    view user routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import AddUser from './users/AddUser';
 import EditUser from './users/EditUser';
+import ViewUser from './users/ViewUser';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path='/' element={<Home />}></Route>
           <Route exact path='/adduser' element={<AddUser />}></Route>
           <Route exact path='/edituser/:id' element={<EditUser />}></Route>
+          <Route exact path='/viewuser/:id' element={<ViewUser />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/users/ViewUser.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function ViewUser() {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-md-6 offset-md-3 border rounded mt-2 shadow'>
+                <h2 className="text-center m-4">User Details</h2>
+                <div className='card'>
+                    <div className='card-reader'>
+                        Details of user id:
+                        <ul className='list-group list-group-flush'>
+                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
