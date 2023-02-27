165e07f1f538fc1bb73ddefe1519c71efee819fb
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-02-27 08:54:22 -0800

    view user details and complete application

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,9 +6,9 @@ export default function navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" href="#" to={"/"}>
             Full Stack Application
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
